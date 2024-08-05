8e29ba1a2321c5da37f48487009882532ad18005
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2024-08-05 10:02:47 +0300

    Update

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
-# For course staff only. Update homework.
+# For course staff only. Updates homework in template repo.
+# Expects: remote origin - homework template, remote template - internal template.
 # Example: sh update.sh "Commit message"
 set -e
 
 git push origin main:main # To have main as a default branch on github (first push)
 
 git checkout solutions
-git pull upstream main
+git pull template main
 sh check.sh "" "-Wunused-imports -Wredundant-constraints"
 if [ "$1" = "" ]
 then
@@ -19,7 +20,6 @@ git push origin solutions:solutions
 git checkout main
 git merge solutions -m "[no ci] Merged with solutions"
 sh check-build.sh
-
 git push origin main:main
-git push public main:main
+
 git checkout solutions
