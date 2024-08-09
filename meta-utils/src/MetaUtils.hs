7ac7a7a141c45b0607e93596056f32cd043b4888
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2024-08-09 10:37:16 +0300

    Update

--- meta-utils/src/MetaUtils.hs
+++ meta-utils/src/MetaUtils.hs
@@ -1,12 +1,14 @@
 -- | Фасад пакета.
 module MetaUtils
   ( module ClassDispatcher
+  , module Data.Kind
   , module PP
   , module TodoException
   , module TodoMacro
   ) where
 
 import ClassDispatcher
+import Data.Kind
 import PP
 import TodoException
 import TodoMacro
